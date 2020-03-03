4f4efdc3070c920abb8278f5c562044a27d4e4cc
xjl1995822 <18279148786@163.com>
2020-03-03 10:44:18 +0800

    XJL 20200303

---
+++ src/difficult/countDigitOne.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2018-2020
+ * FileName: countDigitOne
+ * Author:   xjl
+ * Date:     2020/3/3 10:01
+ * Description: 给定一个整数 n，计算所有小于等于 n 的非负整数中数字 1 出现的个数。
+ */
+package difficult;
+
+/**
+ * 输入: 13
+ * 输出: 6
+ * 解释: 数字 1 出现在以下数字中: 1, 10, 11, 12, 13
+ */
+public class countDigitOne {
+    public static int countDigitOne(int n) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/simple_difficulty/isPalindrome2.java
+++ src/simple_difficulty/isPalindrome2.java
@@ -7,29 +7,36 @@
  */
 package simple_difficulty;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 数组顺序遍历和道路遍历能够全部走完表示回文
- * 利用双指针来做
+ * 将字符转为小写且只有字母和数字可以放入
+ * 利用双指针来遍历前后，
+ * 如果全部遍历介绍的话那就是回文
+ * 如果不是遍历完成的话那就回不是回文
+ *
+ * 这里学习到的是list的遍历和Array的排序方法。
  */
 public class isPalindrome2 {
-    public static  boolean isPalindrome(String s) {
-        char[] char1=s.toLowerCase().toCharArray();
-        int i=char1.length-1;
-        int j=0;
-        while (i<j){
-            if(char1[i]==char1[j]){
-                i++;
-                j--;
-            }else {
-                i++;
+    public static boolean isPalindrome(String s) {
+        char[] char1 = s.toLowerCase().toCharArray();
+        List<Character> list=new ArrayList<>();
+        for(int i=0;i<char1.length;i++){
+            if(char1[i]>='a'&&char1[i]<='z'||char1[i]>='0'&&char1[i]<='9'){
+                list.add(char1[i]);
             }
         }
-
-        return true;
-    }
+        for(int i=0;i<list.size()/2;i++){
+            if(list.get(i)!=list.get(list.size()-1-i)){
+                  return false;
+            }
+        }
+        return  true;
+}
 
     public static void main(String[] args) {
-        String s="A man, a plan, a canal: Panama";
+        String s = "race a car";
         System.out.println(isPalindrome(s));
     }
 }
