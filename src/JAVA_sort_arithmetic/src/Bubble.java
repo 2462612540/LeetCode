b21be0d54b87c207c09609f26f611baeeaa6a9c0
xjl1995822 <18279148786@163.com>
2020-03-06 08:56:06 +0800

    XJL 20200306 完成的了冒泡的排序的算法的理解

--- src/JAVA_sort_arithmetic/src/Bubble.java
+++ src/JAVA_sort_arithmetic/src/Bubble.java
@@ -8,6 +8,15 @@
 package JAVA_sort_arithmetic.src;
 
 public class Bubble {
+    /**
+     * 比较和排序的两个重要
+     * 比较的个数是越来越少的所以是从a.length-1 ----0的个数比较。j 是需要进行的比较的个数
+     * Integer 是实现了Comparable的接口的
+     * 最坏的比较的次数是n-1 n-2 n-3……1的次数
+     * 最坏的比较次数是n-1,n-2,n-3^……1的次数
+     *
+     * @param a
+     */
     public static void sort(Comparable[] a) {
         long start = System.currentTimeMillis();
         for (int i = a.length - 1; i > 0; i--) {
--- src/JAVA_sort_arithmetic/test/Sort_Test.java
+++ src/JAVA_sort_arithmetic/test/Sort_Test.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (C), 2018-2020
- * FileName: Bubble_Test
+ * FileName: Sort_Test
  * Author:   xjl
  * Date:     2020/3/6 8:14
  * Description: 冒泡的测试类
@@ -10,7 +10,7 @@ package JAVA_sort_arithmetic.test;
 import JAVA_sort_arithmetic.src.Bubble;
 import org.junit.Test;
 
-public class Bubble_Test {
+public class Sort_Test {
     @Test
     public void bubbleTest() {
         Integer[] nums = new Integer[10000];
