fefe5b61a4a1bb55b1e16032d0fb32aefad0ff97
xjl1995822 <18279148786@163.com>
2020-04-06 08:36:57 +0800

    XJL 20200406

--- src/Leetcode_Medium_difficulty/cuttingRope.java
+++ src/Leetcode_Medium_difficulty/cuttingRope.java
@@ -9,6 +9,9 @@ package Leetcode_Medium_difficulty;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class cuttingRope {
     public int cuttingRope(int n) {
         if (n <= 3) return n - 1;
@@ -19,9 +22,47 @@ public class cuttingRope {
         return (int) Math.pow(3, a) * 2;
     }
 
+    public int cuttingRope2(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int[] f = new int[n + 1];
+        f[1] = 1;
+        f[2] = 2;
+        f[3] = 3;
+        int MAX = 0;
+        for (int i = 4; i <= n; i++) {
+            int max = 0;
+            for (int j = 1; j <= i / 2; j++) {
+                int item = f[j] * f[i - j];
+                if (item > max) {
+                    max = item;
+                }
+                f[i] = max;
+            }
+        }
+        MAX = f[n];
+        return MAX;
+    }
+
+    public int cuttingRope3(int n) {
+        Map map=new HashMap();
+        map.keySet();
+        if (n < 2) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(Math.max(j * dp[i - j], j * (i - j)), dp[i]);
+            }
+        }
+        return dp[n];
+    }
+
     @Test
     public void test() {
-        System.out.println(cuttingRope(8));
+        System.out.println(cuttingRope3(10));
     }
 
 }
