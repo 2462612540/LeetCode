c128bbb89d14585d95f92e69918abc3f887a6ea5
xjl1995822 <18279148786@163.com>
2020-06-10 16:02:01 +0800

    leetcode新增题目
    Signed-off-by: xjl1995822 <18279148786@163.com>

--- src/NewLeetcode/permutation38.java
+++ src/NewLeetcode/permutation38.java
@@ -12,20 +12,11 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 
 public class permutation38 {
-    public String[] permutation(String s) {
-        return null;
-    }
 
-    public ArrayList<String> Permutation(String str) {
-        char[] a = str.toCharArray();
-        ArrayList<String> ans = new ArrayList<>();
-        slove(ans, a, 0, str.length());
-        ans = new ArrayList<String>(new HashSet<String>(ans));
-        Collections.sort(ans);
-        return ans;
-    }
+    List<String> res = new ArrayList<>();
 
     private void slove(ArrayList<String> ans, char[] a, int index, int length) {
         if (index == length - 1) {
@@ -62,4 +53,75 @@ public class permutation38 {
             System.out.println(s);
         }
     }
+
+    public ArrayList<String> Permutation(String str) {
+        char[] a = str.toCharArray();
+        ArrayList<String> ans = new ArrayList<>();
+        slove(ans, a, 0, str.length());
+        ans = new ArrayList<String>(new HashSet<String>(ans));
+        Collections.sort(ans);
+        String[] result = new String[ans.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = ans.get(i);
+        }
+        return ans;
+    }
+
+    public String[] permutation(String s) {
+        char[] chars = s.toCharArray();
+        boolean[] visit = new boolean[chars.length];
+
+        List<Character> list = new ArrayList<>();
+
+        help(chars, 0, list, visit);
+        //处重复
+        List<String> res1 = new ArrayList<>();
+        for (int i = 0; i < res.size(); i++) {
+            if (!res1.contains(res.get(i))) {
+                res1.add(res.get(i));
+            }
+        }
+        //返回的结果
+        String[] result = new String[res1.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = res1.get(i);
+        }
+        return result;
+    }
+
+    private void help(char[] chars, int index, List<Character> list, boolean[] visit) {
+
+        if (list.size() == chars.length) {
+            res.add(test1(list));
+            return;
+        } else {
+            for (int i = 0; i < chars.length; i++)
+                if (!visit[i]) {
+                    //访问
+                    visit[i] = true;
+                    list.add(chars[i]);
+                    help(chars, index + 1, list, visit);
+                    //回溯
+                    visit[i] = false;
+                    list.remove(list.size() - 1);
+                }
+        }
+    }
+
+    private String test1(List<Character> list) {
+        String s = "";
+        for (char c : list) {
+            s += c;
+        }
+        return s;
+    }
+
+    @Test
+    public void test1() {
+        String[] abcs = permutation("aab");
+        for (String s : abcs) {
+            System.out.println(s);
+        }
+    }
+
 }
