739720d436ce2929eb568ced0ab6b0e9d3c2d767
xjl1995822 <18279148786@163.com>
2020-08-10 09:40:37 +0800

    一个数的平方根的算法

---
+++ src/Tree/MaxTwonumber.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C), 2018-2020
+ * FileName: MaxTwonumber
+ * Author:   xjl
+ * Date:     2020/8/8 10:48
+ * Description: 最远的距离
+ */
+package Tree;
+
+public class MaxTwonumber {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int va) {
+            this.val = va;
+        }
+    }
+
+}
