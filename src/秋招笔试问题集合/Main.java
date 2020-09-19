f9c434504c971ac1d2e53d0216a63bb41984070f
xjl1995822 <18279148786@163.com>
2020-09-19 22:25:11 +0800

    20200919

--- src/秋招笔试问题集合/Main.java
+++ src/秋招笔试问题集合/Main.java
@@ -7,6 +7,34 @@
  */
 package 秋招笔试问题集合;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+        int num = 0;
+        while (list.size() != 1) {
+            for (int i = 0; i < list.size(); i++) {
+                if (++num == m) {
+                    list.remove(list.get(i));
+                    num = 1;
+                }
+            }
+            for (int i = list.size() - 1; i >= 0; i--) {
+                if (++num == m) {
+                    list.remove(list.get(i));
+                    num = 1;
+                }
+            }
+        }
+        System.out.println(list.get(0));
+    }
 }
