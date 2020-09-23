ddadc4ef1119b1aa8be842718e2a9d7679678d4f
xjl1995822 <18279148786@163.com>
2020-09-23 16:43:56 +0800

    20200923

--- src/秋招笔试问题集合/Main.java
+++ src/秋招笔试问题集合/Main.java
@@ -7,60 +7,6 @@
  */
 package 秋招笔试问题集合;
 
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();//大小
-        for (int i = 0; i < n; i++) {
-            int m = sc.nextInt();
-            int[] array1 = new int[m];
-            int[] array2 = new int[m];
-            for (int j = 0; j < m; j++) {
-                array1[j] = sc.nextInt();
-            }
-            for (int j = 0; j < m; j++) {
-                array2[j] = sc.nextInt();
-            }
-            boolean res = test11(array1, array2);
-            if (res){
-                System.out.println("YES");
-            }else {
-                System.out.println("NO");
-            }
-        }
-    }
 
-    private static boolean test11(int[] array1, int[] array2) {
-        //array1表示的是原来的   array2表示的新的
-        for (int i = 0; i < array2.length; i++) {
-            if (array1[i] == array2[i]) {
-                continue;
-            } else {
-                int index = -1;
-                //找到这个位置
-                for (int k = 0; k < array1.length; k++) {
-                    if (array1[k] == array2[i]) {
-                        index = k;
-                        break;
-                    }
-                }
-                if (index < 0) {
-                    return false;
-                }
-                //只能是从倒置回来
-                if (index >= i) {
-                    while (index >= i) {
-                        array1[index--] = array2[i];
-                    }
-                } else {
-                    while (index <= i) {
-                        array1[index++] = array2[i];
-                    }
-                }
-            }
-        }
-        return true;
-    }
 }
