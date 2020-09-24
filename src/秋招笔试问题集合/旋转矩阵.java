d6a83e38c8a3a97876f8409417216080d638ae51
xjl1995822 <18279148786@163.com>
2020-09-24 10:11:05 +0800

    20200923

--- src/秋招笔试问题集合/旋转矩阵.java
+++ src/秋招笔试问题集合/旋转矩阵.java
@@ -23,16 +23,16 @@ public class 旋转矩阵 {
         int total = rows * colums;
         int row = 0, colum = 0;
         int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int directionindex = 0;
+        int index = 0;
         for (int i = 0; i < total; i++) {
             order.add(matrix[row][colum]);
             visited[row][colum] = true;
-            int nextRow = row + directions[directionindex][0], nexColum = colum + directions[directionindex][1];
+            int nextRow = row + directions[index][0], nexColum = colum + directions[index][1];
             if (nextRow < 0 || nextRow >= rows || nexColum < 0 || nexColum >= colums || visited[nextRow][nexColum]) {
-                directionindex = (directionindex + 1) % 4;
+                index = (index + 1) % 4;
             }
-            row += directions[directionindex][0];
-            colum += directions[directionindex][1];
+            row += directions[index][0];
+            colum += directions[index][1];
         }
         int[] ans = new int[order.size()];
         for (int i = 0; i < ans.length; i++) {
