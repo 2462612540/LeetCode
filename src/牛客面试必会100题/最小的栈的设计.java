9ed774715c8c49d7c2725013b4acb1b768576fa3
xjl1995822 <18279148786@163.com>
2020-09-29 22:21:37 +0800

    20200929

---
+++ src/牛客面试必会100题/最小的栈的设计.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright (C), 2018-2020
+ * FileName: 最小的栈的设计
+ * Author:   xjl
+ * Date:     2020/9/29 10:12
+ * Description:
+ */
+package 牛客面试必会100题;
+
+public class 最小的栈的设计 {
+
+}
---
+++ src/牛客面试必会100题/连续的子数组的最大和.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C), 2018-2020
+ * FileName: 连续的子数组的最大和
+ * Author:   xjl
+ * Date:     2020/9/29 21:33
+ * Description:
+ */
+package 牛客面试必会100题;
+
+import org.junit.Test;
+
+public class 连续的子数组的最大和 {
+
+    public int FindGreatestSumOfSubArray(int[] array) {
+        int res = array[0];
+        int[] dp = new int[array.length];
+        dp[0] = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (dp[i-1] >= 0) {
+                dp[i] = dp[i - 1] + array[i];
+            } else {
+                dp[i] = array[i];
+            }
+            if (dp[i]>res){
+                res=dp[i];
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public void test() {
+        int[] array = {-2,-8,-1,-5,-9};
+        int res = FindGreatestSumOfSubArray(array);
+        System.out.println(res);
+    }
+}
