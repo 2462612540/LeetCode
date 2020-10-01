e872e52299d8d6846426684af2b9dffffd9b9fd7
xjl1995822 <18279148786@163.com>
2020-10-01 12:42:37 +0800

    20201001

--- src/数学问题/两个数相加.java
+++ src/数学问题/两个数相加.java
@@ -7,5 +7,41 @@
  */
 package 数学问题;
 
+import org.junit.Test;
+
 public class 两个数相加 {
+
+    @Test
+    public void test() {
+        String s = addStrings("99", "99");
+        System.out.println(s);
+    }
+
+    public String addStrings(String num1, String num2) {
+        String result = "";
+        int curr = 0;
+        int n1 = num1.length() - 1;
+        int n2 = num2.length() - 1;
+        int x = 0, y = 0;
+        while (n1 >= 0 || n2 >= 0) {
+            if (n1 < 0) {
+                x = 0;
+            } else {
+                x = num1.charAt(n1) - '0';
+            }
+            if (n2 < 0) {
+                y = 0;
+            } else {
+                y = num2.charAt(n2) - '0';
+            }
+            result += String.valueOf((x + y + curr) % 10);
+            curr = (x + y + curr) / 10;
+            n1--;
+            n2--;
+        }
+        if (curr == 1) {
+            result += "1";
+        }
+        return new StringBuffer(result).reverse().toString();
+    }
 }
--- src/牛客面试必会100题/大数相乘.java
+++ src/牛客面试必会100题/大数相乘.java
@@ -7,13 +7,46 @@
  */
 package 牛客面试必会100题;
 
+import org.junit.Test;
+
 import java.math.BigDecimal;
 
 public class 大数相乘 {
 
-    public String solve1(String s, String t) {
+    @Test
+    public void test() {
+        String s = solve1("3", "555");
+        System.out.println(s);
+    }
 
-        return null;
+    public String solve1(String s, String t) {
+        if (s.equals("0") || t.equals("0"))
+            return "0";
+        int len1 = s.length();
+        int len2 = t.length();
+        //两个数字相乘的结果的位数的是两个数的和
+        int[] ans = new int[len1 + len2];
+        //计算的是每一个的位置的上的结果
+        for (int i = len1 - 1; i >= 0; i--) {
+            int x = s.charAt(i) - '0';
+            for (int j = len2 - 1; j >= 0; j--) {
+                int y = t.charAt(j) - '0';
+                ans[i + j + 1] += x * y;
+            }
+        }
+        //计算统一来进行的进位计算
+        for (int i = len1 + len2 - 1; i > 0; i--) {
+            ans[i - 1] += ans[i] / 10;
+            ans[i] %= 10;
+        }
+
+        StringBuilder res = new StringBuilder();
+        int index = ans[0] == 0 ? 1 : 0;
+        while (index < len1 + len2) {
+            res.append(ans[index]);
+            index++;
+        }
+        return res.toString();
     }
 
     /**
