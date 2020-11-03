d2976e1873d19e844afb450b7c20b62f6d5fe6e4
xjl1995822 <18279148786@163.com>
2020-11-03 15:06:56 +0800

    20201103

--- src/数组问题/买卖股票的最佳时机121.java
+++ src/数组问题/买卖股票的最佳时机121.java
@@ -8,4 +8,16 @@
 package 数组问题;
 
 public class 买卖股票的最佳时机121 {
+
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] <= min) {
+                min = prices[i];
+            }
+            max = Math.max(prices[i] - min, max);
+        }
+        return max;
+    }
 }
