ffe39dc3537355720e93bdde878df4c74e02e0c3
xjl1995822 <18279148786@163.com>
2020-12-08 20:20:39 +0800

    牛客题目面试必考题

---
+++ 牛客网/复现代码/二叉树的直径.java
@@ -0,0 +1,47 @@
+package 复现代码;
+
+/**
+ * @Classname 二叉树的直径
+ * @Description TODO
+ * @Date 2020/12/8 16:09
+ * @Created by xjl
+ */
+public class 二叉树的直径 {
+    //表示二叉数据的直径 表示的是如果存在话至少1的
+    int dis = 1;
+
+    /**
+     * @description TODO 表示的是的二叉树的直径
+     * @param: root
+     * @date: 2020/12/8 16:10
+     * @return: int
+     * @author: xjl
+     */
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        deepth(root);
+        return dis - 1;
+    }
+
+    private int deepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = deepth(root.left);
+        int right = deepth(root.right);
+        dis = Math.max(dis, left + right + 1);
+        return Math.max(left, right) + 1;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
