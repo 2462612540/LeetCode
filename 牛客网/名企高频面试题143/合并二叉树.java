ce5f0cee3145d11b9421b62b1b503c1e0a8fbc16
xjl1995822 <18279148786@163.com>
2020-12-13 22:12:23 +0800

    牛客网143

---
+++ 牛客网/名企高频面试题143/合并二叉树.java
@@ -0,0 +1,55 @@
+package 名企高频面试题143;
+
+/**
+ * @Classname 合并二叉树
+ * @Description TODO
+ * @Date 2020/12/13 20:32
+ * @Created by xjl
+ */
+public class 合并二叉树 {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        t1.val = t1.val + t2.val;
+        if (t1.left != null && t2.left != null) {
+            mergeTrees(t1.left, t2.left);
+        } else if (t1.left == null && t2.left != null) {
+            t1.left = t2.left;
+        }
+        if (t1.right != null && t2.right != null) {
+            mergeTrees(t1.right, t2.right);
+        } else if (t1.right == null && t2.right != null) {
+            t1.right = t2.right;
+        }
+        return t1;
+    }
+
+    /**
+     * @description TODO 代码的复现代码
+     * @param: t1
+     * @param: t2
+     * @date: 2020/12/13 21:01
+     * @return: 名企高频面试题143.合并二叉树.TreeNode
+     * @author: xjl
+     */
+    public TreeNode mergeTrees1(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        t1.val += t2.val;
+        if (t1.left != null && t2.left != null) {
+            mergeTrees(t1.left, t2.left);
+        } else if (t1.left == null && t2.left != null) {
+            t1.left = t2.left;
+        }
+        if (t1.right != null && t2.right != null) {
+            mergeTrees(t1.right, t2.right);
+        } else if (t1.right == null && t2.right != null) {
+            t1.right = t2.right;
+        }
+        return t1;
+    }
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+    }
+}
---
+++ 牛客网/复现代码/BST.java
@@ -0,0 +1,40 @@
+package 复现代码;
+
+/**
+ * @Classname BST
+ * @Description TODO
+ * @Date 2020/12/13 14:56
+ * @Created by xjl
+ */
+public class BST {
+    public TreeNode sortArrayBST(int[] nums) {
+        if (nums.length == 0) return null;
+        return builder(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode builder(int[] nums, int start, int end) {
+        //终止条件
+        if (start > end) {
+            return null;
+        }
+        if (start == end) {
+            return new TreeNode(nums[start]);
+        }
+        //实际上这个+1都是可以的。最后的都是一个二叉树
+        int mid = start + (end - start) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = builder(nums, start, mid - 1);
+        root.right = builder(nums, mid + 1, end);
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
