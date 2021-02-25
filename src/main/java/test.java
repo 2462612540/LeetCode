dede138922c5c7a4109076fcbaa146d9ea63e92c
xjl <18279148786@163.com>
2021-02-25 20:26:03 +0800

    update code project finish

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * @Classname test
+ * @Description TODO
+ * @Date 2021/2/25 20:22
+ * @Created by xjl
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hahh");
+    }
+}
