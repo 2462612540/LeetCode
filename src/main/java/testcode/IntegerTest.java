7664d79f8ddf490a239fd591ca1e9e3c58a4bd12
xjl <18279148786@163.com>
2021-03-21 09:28:52 +0800

    code priciating

---
+++ src/main/java/testcode/IntegerTest.java
@@ -0,0 +1,18 @@
+package testcode;
+
+/**
+ * @Classname IntegerTest
+ * @Description TODO
+ * @Date 2021/3/16 14:01
+ * @Created by xjl
+ */
+public class IntegerTest {
+    public static void main(String[] args) {
+        Integer a = 10000, b = 10000;
+        System.out.println(a == b);
+        Integer c = 100, d = 100;
+        System.out.println(c == d);
+
+        String str="";
+    }
+}
