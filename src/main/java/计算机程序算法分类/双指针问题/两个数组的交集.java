d3a495cf5ef146c058f8f72053f54d874c804375
xjl <18279148786@163.com>
2021-04-13 14:01:12 +0800

    二叉树的构造问题

--- src/main/java/计算机程序算法分类/双指针问题/两个数组的交集.java
+++ src/main/java/计算机程序算法分类/双指针问题/两个数组的交集.java
@@ -21,27 +21,16 @@ public class 两个数组的交集 {
         List<Integer> list = new ArrayList<>();
         while (i < nums1.length && j < nums2.length) {
             if (nums1[i] < nums2[j]) {
-                // 如果i指向的值小于j指向的值，，说明i指向
-                // 的值小了，i往后移一步
                 i++;
             } else if (nums1[i] > nums2[j]) {
-                // 如果i指向的值大于j指向的值，说明j指向的值
-                // 小了，j往后移一步
                 j++;
             } else {
-                // 如果i和j指向的值相同，说明这两个值是重复的，
-                // 把他加入到集合list中，然后i和j同时都往后移一步
                 list.add(nums1[i]);
                 i++;
                 j++;
             }
         }
         //把list转化为数组
-        int index = 0;
-        int[] res = new int[list.size()];
-        for (int k = 0; k < list.size(); k++) {
-            res[index++] = list.get(k);
-        }
-        return res;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
