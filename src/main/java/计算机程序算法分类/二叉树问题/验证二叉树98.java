c3e4b69f07c45513fb1b88e1e0850a988396abea
xjl <18279148786@163.com>
2021-05-04 19:45:45 +0800

    二叉树问题

---
+++ src/main/java/计算机程序算法分类/二叉树问题/验证二叉树98.java
@@ -0,0 +1,38 @@
+package 计算机程序算法分类.二叉树问题;
+
+/**
+ * @Classname 验证二叉树98
+ * @Description TODO
+ * @Date 2021/5/4 17:09
+ * @Created by xjl
+ */
+public class 验证二叉树98 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    TreeNode prev;
+
+    public boolean isValidBST3(TreeNode root) {
+        if (root == null)
+            return true;
+        //访问左子树
+        if (!isValidBST3(root.left))
+            return false;
+        //访问当前节点：如果当前节点小于等于中序遍历的前一个节点直接返回false。
+        if (prev != null && prev.val >= root.val)
+            return false;
+        //然后记录这个的根节点
+        prev = root;
+        //访问右子树
+        if (!isValidBST3(root.right))
+            return false;
+        return true;
+    }
+}
